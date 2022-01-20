3d9b0afb8398762eea7bc606d43e9b623805ab03
Alexej <ilinov86@yandex.ru>
2022-01-20 10:24:24 +0300

    rm err

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Package tcpserverweb - модуль управления cookie и вспомагательные функции для пакета tcpserver
+github.com/ilinovalex86/tcpserver
---
+++ tcpserverweb.go
@@ -0,0 +1,3 @@
+// Package tcpserverweb - модуль управления cookie и вспомагательные функции для пакета tcpserver
+// github.com/ilinovalex86/tcpserver
+package tcpserverweb
