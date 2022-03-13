33acf4c96667cadff1b69d7ef57250f797e0bcd7
Alexej <ilinov86@yandex.ru>
2022-03-13 18:02:50 +0300

    rm err

--- generatorId.go
+++ generatorId.go
@@ -11,6 +11,9 @@ func GeneratorId() string {
 	rand.Seed(time.Now().UnixNano())
 	for {
 		id := strconv.Itoa(rand.Int())
+		if len(id) < 19 {
+			continue
+		}
 		if Clients.exist(id) {
 			continue
 		} else {
